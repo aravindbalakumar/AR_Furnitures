670f9a7b87b1eac8f5b4087b52cc5c906cb2973d
aravindbalakumar <101953156+aravindbalakumar@users.noreply.github.com>
2024-11-21 16:43:44 +0000

    InitialFixes
    InitialFixes

--- Assets/Scripts/FurnitureScriptable.cs
+++ Assets/Scripts/FurnitureScriptable.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Furn_Instance : ScriptableObject
 {
     public string Furn_ID;
-    public Category_Enum Furn_Category_ID;
     public string Furn_Name;
+    public Category_Enum Furn_Category;
     public Sprite Furn_Image;
     public GameObject Furn_Prefab;
 }
@@ -15,20 +15,10 @@ public class Furn_Instance : ScriptableObject
 public class Furn_Data : ScriptableObject
 {
     public List<Furn_Instance> totalFurnitures;
-    public HashSet<Furn_Category> totalCategories;
 }
 
-[CreateAssetMenu(fileName ="Furn_Category",menuName ="Furniture/Furn_Category",order =3)]
-public class Furn_Category:ScriptableObject
-{
-    public Category_Enum Category_ID;
-    public string Category_Name;
-    public Sprite Category_Sprite;
-}
 
 public enum Category_Enum
 {
-    ctg_ch,//for chairs
-    ctg_sof,//for sofas
-    ctg_bed//for bed
+    Chairs,Racks,Tables,Bed,Sofa,Others
 }
